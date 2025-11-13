fcc56dc9080fd7a05664233f165570b3ef857ae1
Mar Mar <marmar@Mars-MBP.attlocal.net>
2025-11-12 22:47:24 -0600

    UserSync added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,14 @@ import WhatToAsk from "@/components/landing/WhatToAsk";
 import PricingSection from "@/components/landing/PricingSection";
 import CTA from "@/components/landing/CTA";
 import Footer from "@/components/landing/Footer";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
+export default async function Home() {
+    const user = await currentUser()
 
-export default function Home() {
+    // redirect the auth user to dashboard
+    if (user) redirect("/dashboard");
   return (
     <div className="min-h-screen bg-background">
       <Header />
